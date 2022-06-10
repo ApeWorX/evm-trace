603a7b7d559349ebd04fc7a973402b3bc7de686a
unparalleled-js <yingthi@live.com>
2022-06-10 11:37:08 -0500

    feat: export new method

--- evm_trace/__init__.py
+++ evm_trace/__init__.py
@@ -1,3 +1,10 @@
 from .base import CallTreeNode, CallType, TraceFrame, get_calltree_from_trace
+from .parity import get_calltree_from_parity_trace
 
-__all__ = ["CallTreeNode", "CallType", "get_calltree_from_trace", "TraceFrame"]
+__all__ = [
+    "CallTreeNode",
+    "CallType",
+    "get_calltree_from_trace",
+    "get_calltree_from_parity_trace",
+    "TraceFrame",
+]
