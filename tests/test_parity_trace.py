3c34090c56b4632b3ccf80021477f2e3b5ca1eea
banteg <banteeg@gmail.com>
2022-06-11 05:40:25 +0400

    test: update expected traces and tests

--- tests/test_parity_trace.py
+++ tests/test_parity_trace.py
@@ -1,16 +1,15 @@
-import json
 from pathlib import Path
-from typing import Dict, List
 
 import pytest
 
-from evm_trace.parity import ParityTrace, get_calltree_from_parity_trace
+from evm_trace.parity import ParityTraceList, get_calltree_from_parity_trace
 
 from .expected_traces import (
     PARITY_CALL_TRACE_EXPECTED_OUTPUT,
     PARITY_CREATE_TRACE_EXPECTED_OUTPUT,
     PARITY_SELFDESTRUCT_TRACE_EXPECTED_OUTPUT,
     PARITY_REVERT_TRACE_EXPECTED_OUTPUT,
+    PARITY_OUT_OF_GAS_TRACE_EXPECTED_OUTPUT,
 )
 
 DATA_PATH = Path(__file__).parent / "data" / "parity"
@@ -19,13 +18,14 @@ EXPECTED_OUTPUT_MAP = {
     "create": PARITY_CREATE_TRACE_EXPECTED_OUTPUT,
     "selfdestruct": PARITY_SELFDESTRUCT_TRACE_EXPECTED_OUTPUT,
     "revert": PARITY_REVERT_TRACE_EXPECTED_OUTPUT,
+    "error": PARITY_OUT_OF_GAS_TRACE_EXPECTED_OUTPUT,
 }
 
 
-@pytest.mark.parametrize("name", ("call", "create", "selfdestruct", "revert"))
+@pytest.mark.parametrize("name", ("call", "create", "selfdestruct", "revert", "error"))
 def test_parity(name):
-    raw_data: List[Dict] = json.loads((DATA_PATH / f"{name}.json").read_text())
-    traces = [ParityTrace.parse_obj(d) for d in raw_data]
-    actual = get_calltree_from_parity_trace(traces[0], traces)
+    path = DATA_PATH / f"{name}.json"
+    traces = ParityTraceList.parse_file(path)
+    actual = get_calltree_from_parity_trace(traces)
     expected = EXPECTED_OUTPUT_MAP[name].strip()
     assert repr(actual) == expected
