f86567f862bda1af0813bd0fffc4e757d9f6f84b
banteg <banteeg@gmail.com>
2022-06-11 04:10:15 +0400

    test: add revert trace test case

--- tests/test_parity_trace.py
+++ tests/test_parity_trace.py
@@ -10,6 +10,7 @@ from .expected_traces import (
     PARITY_CALL_TRACE_EXPECTED_OUTPUT,
     PARITY_CREATE_TRACE_EXPECTED_OUTPUT,
     PARITY_SELFDESTRUCT_TRACE_EXPECTED_OUTPUT,
+    PARITY_REVERT_TRACE_EXPECTED_OUTPUT,
 )
 
 DATA_PATH = Path(__file__).parent / "data" / "parity"
@@ -17,10 +18,11 @@ EXPECTED_OUTPUT_MAP = {
     "call": PARITY_CALL_TRACE_EXPECTED_OUTPUT,
     "create": PARITY_CREATE_TRACE_EXPECTED_OUTPUT,
     "selfdestruct": PARITY_SELFDESTRUCT_TRACE_EXPECTED_OUTPUT,
+    "revert": PARITY_REVERT_TRACE_EXPECTED_OUTPUT,
 }
 
 
-@pytest.mark.parametrize("name", ("call", "create", "selfdestruct"))
+@pytest.mark.parametrize("name", ("call", "create", "selfdestruct", "revert"))
 def test_parity(name):
     raw_data: List[Dict] = json.loads((DATA_PATH / f"{name}.json").read_text())
     traces = [ParityTrace.parse_obj(d) for d in raw_data]
