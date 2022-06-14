e7b0ef0edc9b96b672717adf976088c0d336b22a
unparalleled-js <yingthi@live.com>
2022-06-14 15:40:42 -0500

    feat: add test for revert message

--- tests/test_parity_trace.py
+++ tests/test_parity_trace.py
@@ -10,6 +10,7 @@ from .expected_traces import (
     PARITY_CREATE_TRACE_EXPECTED_OUTPUT,
     PARITY_OUT_OF_GAS_TRACE_EXPECTED_OUTPUT,
     PARITY_REVERT_TRACE_EXPECTED_OUTPUT,
+    PARITY_REVERT_TRACE_WITH_MESSAGE_EXPECTED_OUTPUT,
     PARITY_SELFDESTRUCT_TRACE_EXPECTED_OUTPUT,
 )
 
@@ -21,15 +22,19 @@ EXPECTED_OUTPUT_MAP = {
     "revert": PARITY_REVERT_TRACE_EXPECTED_OUTPUT,
     "error": PARITY_OUT_OF_GAS_TRACE_EXPECTED_OUTPUT,
     "create_revert": PARITY_CREATE_REVERT_TRACE_EXPECTED_OUTPUT,
+    "revert_with_message": PARITY_REVERT_TRACE_WITH_MESSAGE_EXPECTED_OUTPUT,
 }
 
 
 @pytest.mark.parametrize(
-    "name", ("call", "create", "selfdestruct", "revert", "error", "create_revert")
+    "name",
+    ("call", "create", "selfdestruct", "revert", "error", "create_revert", "revert_with_message"),
 )
 def test_parity(name):
+    assert name in EXPECTED_OUTPUT_MAP, f"Missing expected output set for '{name}'."
     path = DATA_PATH / f"{name}.json"
     traces = ParityTraceList.parse_file(path)
     actual = repr(get_calltree_from_parity_trace(traces))
+
     expected = EXPECTED_OUTPUT_MAP[name].strip()
     assert actual == expected
