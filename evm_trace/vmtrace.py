de7aadd1c592bb2f923ee603c90b6c746878a7f2
banteg <4562643+banteg@users.noreply.github.com>
2022-07-23 13:14:35 +0000

    style: rename

--- evm_trace/vmtrace.py
+++ evm_trace/vmtrace.py
@@ -10,9 +10,9 @@ from hexbytes import HexBytes
 from msgspec import Struct
 from msgspec.json import Decoder
 
-# fmt: off
 # opcodes grouped by the number of items they pop from the stack
-POPS_TO_CODE = {
+# fmt: off
+POP_OPCODES = {
     1: ["EXTCODEHASH", "ISZERO", "NOT", "BALANCE", "CALLDATALOAD", "EXTCODESIZE", "BLOCKHASH", "POP", "MLOAD", "SLOAD", "JUMP", "SELFDESTRUCT"],  # noqa: E501
     2: ["SHL", "SHR", "SAR", "REVERT", "ADD", "MUL", "SUB", "DIV", "SDIV", "MOD", "SMOD", "EXP", "SIGNEXTEND", "LT", "GT", "SLT", "SGT", "EQ", "AND", "XOR", "OR", "BYTE", "SHA3", "MSTORE", "MSTORE8", "SSTORE", "JUMPI", "LOG0", "RETURN"],  # noqa: E501
     3: ["RETURNDATACOPY", "ADDMOD", "MULMOD", "CALLDATACOPY", "CODECOPY", "CREATE"],
@@ -21,7 +21,7 @@ POPS_TO_CODE = {
     7: ["CALL", "CALLCODE"]
 }
 # fmt: on
-POPCODES = {op: n for n, opcodes in POPS_TO_CODE.items() for op in opcodes}
+POPCODES = {op: n for n, opcodes in POP_OPCODES.items() for op in opcodes}
 POPCODES.update({f"LOG{n}": n + 2 for n in range(1, 5)})
 POPCODES.update({f"SWAP{i}": i + 1 for i in range(1, 17)})
 POPCODES.update({f"DUP{i}": i for i in range(1, 17)})
