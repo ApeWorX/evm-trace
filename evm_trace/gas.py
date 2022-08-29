d564a3f2d1626fc8af086827a791fe9ca7ba1766
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2022-08-29 11:29:03 -0400

    docs: add docstrings

--- evm_trace/gas.py
+++ evm_trace/gas.py
@@ -10,6 +10,15 @@ GasReport = Dict[Address, Dict[Calldata, List[GasUsage]]]
 
 
 def extract_gas_report(calltree: CallTreeNode) -> GasReport:
+    """
+    Extracts a gas report object from a :class:`~evm_trace.base.CallTreeNode`
+
+    Args:
+        calltree (:class:`~evm_trace.base.CallTreeNode`): extractable call tree
+
+    Returns:
+        :class:`~evm_trace.gas.GasReport`: Gas report structure from a call tree.
+    """
     report = {
         calltree.address: {calltree.calldata: [calltree.gas_cost] if calltree.gas_cost else []}
     }
@@ -21,6 +30,16 @@ def extract_gas_report(calltree: CallTreeNode) -> GasReport:
 
 
 def combine_gas_reports(report1: GasReport, report2: GasReport) -> GasReport:
+    """
+    Combines two gas report objects into a single gas report. :class:`~evm_trace.base.CallTreeNode`
+
+    Args:
+        report1 (:class:`~evm_trace.gas.GasReport`): the first instance of GasReport
+        report2 (:class:`~evm_trace.gas.GasReport`): the second instance of GasReport
+
+    Returns:
+        :class:`~evm_trace.gas.GasReport`: A combined gas report structure.
+    """
 
     for address, calldata_dict in report2.items():
         if address in report1:
